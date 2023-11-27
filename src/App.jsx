51711b3c7ddf0aebc3acfc21a46aba3e1c338953
Jhonjandrysdev <jhonjandrysdramirez@unicesar.edu.co>
2023-11-26 21:01:15 -0500

    Se genero logica para calcular actividades o medidas incongruentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import Prueba from "./components/Prueba";
 import TituloTabla from "./components/TituloTabla";
 const App = () => {
+
+  const [datos, setDatos] = useState([]);
+  const [sospechoso, setSospechoso] = useState([])
+
+  const averageHeartRateHigh = 201;
+  const averageHeartRate = 149.2;
+  const averageHeartRateLow = 90;
+// Medidas ritmo cardiaco
+
+  const averageDurationHigh = 127271
+  const averageDuration = 2775.86;
+  const averageDurationLow = 0
+ // Medidas tiempo por segundo 
+
+ const averageDistanceInMetersHigh = 116827.43
+ const averageDistanceInMeters = 6494.885;
+  const averageDistanceInMetersLow = 0;
+  //Medidas de distancia en metros
+
+
+useEffect(() => {
   const data = [
     {
       Id: 1,
       UserId: 10,
       StartTimeInSeconds: 1665239344,
       DurationInSeconds: 1500,
-      DistanceInMeters: 4442.99,
+      DistanceInMeters: 234234234324234,
       Steps: 4224,
       AverageSpeedInMetersPerSecond: 2.962,
       AveragePaceInMinutesPerKilometer: 5.626829,
@@ -24,14 +46,14 @@ const App = () => {
       AverageSpeedInMetersPerSecond: 2.924,
       AveragePaceInMinutesPerKilometer: 5.6999545,
       TotalElevationGainInMeters: 80.48,
-      AverageHeartRateInBeatsPerMinute: 162,
+      AverageHeartRateInBeatsPerMinute: 0,
     },
     {
       Id: 6,
       UserId: 10,
       StartTimeInSeconds: 1667740865,
-      DurationInSeconds: 1800,
-      DistanceInMeters: 5311.3,
+      DurationInSeconds: 0,
+      DistanceInMeters: 123.3,
       Steps: 5084,
       AverageSpeedInMetersPerSecond: 2.951,
       AveragePaceInMinutesPerKilometer: 5.6478033,
@@ -76,6 +98,56 @@ const App = () => {
     },
   ];
 
+  setDatos(data)
+}, [])
+
+const getEstiloFila = (actividad) => {
+  const {
+    Id,
+    AverageHeartRateInBeatsPerMinute,
+    DurationInSeconds,
+    DistanceInMeters,
+  } = actividad;
+
+  const dateAverageDistanceInMetersHigh = (DurationInSeconds / 60)
+  const exactlyAverageDistanceInMetersHigh = parseInt(dateAverageDistanceInMetersHigh);
+
+  if(AverageHeartRateInBeatsPerMinute < averageHeartRateLow && AverageHeartRateInBeatsPerMinute > averageHeartRateHigh){
+    console.log("Este rango esta sospechoso")
+  } else{
+    console.log("Tiene buen ritmo cardiaco")
+  }
+
+  if((DurationInSeconds / 60) === averageDurationLow && (DurationInSeconds / 60 ) === averageDurationHigh){
+    console.log("Tiempo sospechoso")
+  } else{
+    console.log("Tiempo adecuado para su actividad")
+  }
+
+  if(exactlyAverageDistanceInMetersHigh === averageDistanceInMetersLow && exactlyAverageDistanceInMetersHigh >= averageDistanceInMetersHigh ){
+    console.log("Distancia irregular")
+  } else{
+    console.log("Distancia adecuada")
+  }
+
+
+  const esSospechosa =
+    AverageHeartRateInBeatsPerMinute === 0 ||
+    DurationInSeconds === 0 ||
+    DistanceInMeters === 0 ||
+    AverageHeartRateInBeatsPerMinute < averageHeartRate / 2 ||
+    DurationInSeconds > averageDuration * 2 ||
+    DistanceInMeters > averageDistanceInMeters * 2;
+
+  if (esSospechosa) {
+    (`Estas actividades presentan incongruencia:  Distancia: ${DistanceInMeters} - Ritmo cardiado : ${AverageHeartRateInBeatsPerMinute} - Duración: ${DurationInSeconds}`);
+  }
+  console.log("Se esta ejecutando")
+
+  return esSospechosa ? { backgroundColor: 'red' } : {};
+};
+  
+
   return (
     <div className="w-full">
       <div className="container mx-auto my-5">
@@ -116,22 +188,24 @@ const App = () => {
                 className="border-r border-gray-800 p-3 text-sm"
               />
               <TituloTabla
-                text="¿Hay trampa?"
-                className="border-gray-800 p-3 text-sm"
+                text="Actividad sospechosa"
+                className="border-r border-gray-800 p-3 text-sm"
               />
             </tr>
           </thead>
           <tbody>
-            {data.map((item) => (
-              <tr key={item.Id} className="border-b border-gray-800">
-                <td>{item.Id}</td>
-                <td>{item.StartTimeInSeconds}</td>
-                <td>{item.DurationInSeconds}</td>
-                <td>{item.DistanceInMeters}</td>
-                <td>{item.Steps}</td>
-                <td>{item.AverageSpeedInMetersPerSecond}</td>
-                <td>{item.AveragePaceInMinutesPerKilometer}</td>
-                <td>{item.AverageHeartRateInBeatsPerMinute}</td>
+            {datos.map((item) => (
+              <tr key={item.Id} className="border-b border-gray-800" style={getEstiloFila(item)} >
+                <td className="border-r  border-gray-800">{item.Id}</td>
+                <td className="border-r  border-gray-800">{item.StartTimeInSeconds}</td>
+                <td className="border-r  border-gray-800">{item.DurationInSeconds}</td>
+                <td className="border-r  border-gray-800">{item.DistanceInMeters}</td>
+                <td className="border-r  border-gray-800">{item.Steps}</td>
+                <td className="border-r  border-gray-800">{item.AverageSpeedInMetersPerSecond}</td>
+                <td className="border-r  border-gray-800">{item.AveragePaceInMinutesPerKilometer}</td>
+                <td className="border-r  border-gray-800">{item.AverageHeartRateInBeatsPerMinute}</td>
+                <td className="border-r  border-gray-800">{}</td>
+
               </tr>
             ))}
           </tbody>
